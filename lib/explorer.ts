48902ca0e24c09faa0c6d163c82d15934b006817
maff1989 <maff1989@stamprepo.local>
2022-06-23 16:29:26 -0500

    lib/explorer.ts: add JSDocs and move get_hashrate() function

--- lib/explorer.ts
+++ lib/explorer.ts
@@ -92,7 +92,12 @@ const rpc = new BitcoinRpc('http://'
   + `${settings.wallet.password}@`
   + `${settings.wallet.host}:`
   + `${settings.wallet.port}`);
-
+/**
+ * Run command with params against Lotus RPC daemon
+ * @param command - RPC command
+ * @param params  - RPC command parameters
+ * @returns {Promise<any>} Command result
+ */
 const rpcCommand = async (command: string, params: Array<string | number | boolean> = []) => {
   try {
     return await rpc.call(command, ...params);
@@ -102,14 +107,95 @@ const rpcCommand = async (command: string, params: Array<string | number | boole
 };
 
 export class Explorer {
-
+  /**
+   * Converts XPI units into satoshi units
+   * @param amount - Amount of XPI
+   * @returns {number} Satoshi units
+   */
   convert_to_satoshi(amount: number): number {
     // Lotus has only 6 decimal places
     const fixed = amount.toFixed(6).toString();
     // remove decimal (.) and return integer
     return parseInt(fixed.replace('.', ''));
   };
-
+  /**
+   * Converts satoshi units into XPI units
+   * @param sats - Amount of satoshis
+   * @returns {number} XPI units
+   */
+  convert_to_xpi(sats: number): number {
+    return sats / XPI_DIVISOR;
+  }
+  /*
+  get_peerinfo: function(cb) {
+    // RPC call goes here
+  },
+  */
+  /**
+   * RPC command - `getdifficulty`
+   * @returns {Promise<number>} Raw difficulty
+   */
+  async get_difficulty(): Promise<number> {
+    return await rpcCommand('getdifficulty');
+  };
+  /**
+   * RPC command - `getconnectioncount`
+   * @returns {Promise<number>} Raw connection count
+   */
+  async get_connectioncount(): Promise<number> {
+    return await rpcCommand('getconnectioncount');
+  };
+  /**
+   * RPC command - `getmempoolinfo`
+   * @returns {Promise<MempoolInfo>} Raw mempool info
+   */
+  async get_mempoolinfo(): Promise<MempoolInfo> {
+    return await rpcCommand('getmempoolinfo');
+  };
+  /**
+   * RPC command - `getrawmempool`
+   * @returns {Promise<string[]>} Array of txids
+   */
+  async get_rawmempool(): Promise<string[]> {
+    return await rpcCommand('getrawmempool', [ true ]);
+  };
+  /**
+   * RPC command - `getblockcount`
+   * @returns {Promise<number>} Raw block count
+   */
+  async get_blockcount(): Promise<number> {
+    return await rpcCommand('getblockcount');
+  };
+  /**
+   * RPC command - `getblockhash`
+   * @param height - Block height
+   * @returns {Promise<string>} Raw block hash
+   */
+  async get_blockhash(height: number): Promise<string> {
+    return await rpcCommand('getblockhash', [ height ]);
+  };
+  /**
+   * RPC command - `getblock`
+   * @param hash - Raw block hash
+   * @returns {Promise<BlockInfo>} Raw block info
+   */
+  async get_block(hash: string): Promise<BlockInfo> {
+    return await rpcCommand('getblock', [ hash ]);
+  };
+  /**
+   * RPC command - `getrawtransaction`
+   * @param txid - Raw transaction ID
+   * @returns {Promise<RawTransaction} Raw transaction info
+   */
+  async get_rawtransaction(txid: string): Promise<RawTransaction> {
+    return await rpcCommand('getrawtransaction', [ txid, true ]);
+  };
+  /**
+   * RPC command - `getmininginfo`
+   * 
+   * Get hashrate in configured unit from RPC daemon
+   * @returns {Promise<string | number>} Converted hashrate, or `-` if configured to not display
+   */
   async get_hashrate(): Promise<string | number> {
     if (settings.index.show_hashrate == false) {
       return '-';
@@ -133,53 +219,21 @@ export class Explorer {
     }
     return 0;
   };
-  /*
-  get_peerinfo: function(cb) {
-    // RPC call goes here
-  },
-  */
-
-  async get_difficulty(): Promise<number> {
-    return await rpcCommand('getdifficulty');
-  };
-
-  async get_connectioncount(): Promise<number> {
-    return await rpcCommand('getconnectioncount');
-  };
-
-  // get basic information about the mempool
-  async get_mempoolinfo(): Promise<MempoolInfo> {
-    return await rpcCommand('getmempoolinfo');
-  };
-
-  // get all transactions in the mempool
-  async get_rawmempool(): Promise<string[]> {
-    return await rpcCommand('getrawmempool', [ true ]);
-  };
-
-  async get_blockcount(): Promise<number> {
-    return await rpcCommand('getblockcount');
-  };
-
-  async get_blockhash(height: number): Promise<string> {
-    return await rpcCommand('getblockhash', [ height ]);
-  };
-
-  async get_block(hash: string): Promise<BlockInfo> {
-    return await rpcCommand('getblock', [ hash ]);
-  };
-
-  async get_rawtransaction(hash: string): Promise<RawTransaction> {
-    return await rpcCommand('getrawtransaction', [ hash, true ]);
-  };
-
+  /**
+   * Fetch all address balances from database and calculate available supply
+   * @returns {Promise<number>} Sum of all address balances in satoshis
+   */
   async balance_supply(): Promise<number> {
     const data = { totalBalance: 0 };
     const docs: AddressDocument[] = await Address.find({}, 'balance').where('balance').gt(0);
     docs.forEach(doc => data.totalBalance += doc.balance);
     return data.totalBalance;
   };
-
+  /**
+   * Fetch all transactions from specified block from database and calculate fees paid/burned
+   * @param height - Block height
+   * @returns Object containing blockFees and blockFeesBurned in satoshis
+   */
   async get_block_fees(height: number): Promise<{
     blockFees: number,
     blockFeesBurned: number,
@@ -190,21 +244,32 @@ export class Explorer {
     data.blockFeesBurned = Math.round(data.blockFees / 2);
     return data;
   };
-
-  // return burned supply in XPI
+  /**
+   * Fetch all blocks from database and calculate burned supply
+   * @returns {Promise<number>} Total burned supply in satoshis
+   */
   async get_burned_supply(): Promise<number> {
     const data = { totalBurned: 0 };
     const docs: BlockDocument[] = await Block.find({}, 'burned').where('burned').gt(0);
     docs.forEach(doc => data.totalBurned += doc.burned);
     return data.totalBurned;
   };
-
+  /**
+   * Fetch the available supply
+   * @returns {Promise<number>} Total supply in XPI
+   */
   async get_supply(): Promise<number> {
     // only supports BALANCE supply
     const supply = await this.balance_supply();
-    return (supply / XPI_DIVISOR);
+    return this.convert_to_xpi(supply);
   };
-
+  /**
+   * Check if block at specified height has been orphaned by the network
+   * 
+   * If block orphaned, check previous blocks to find most recent, non-orphaned block
+   * @param height - Block height
+   * @returns {Promise<number>} Block height of non-orphaned block
+   */
   async is_block_orphaned(height: number): Promise<number> {
     const blockhash = await this.get_blockhash(height);
     const block = await this.get_block(blockhash);
@@ -216,7 +281,11 @@ export class Explorer {
       // calling will determine if processing for orphaned blocks is required
       : height;
   };
-
+  /**
+   * Calculate total input/output value
+   * @param array - Prepared inputs or outputs
+   * @returns {Promise<number>} Total amount in satoshis
+   */
   async calculate_total(
     array: PreparedTransactionInputs | PreparedTransactionOutputs
   ): Promise<number> {
@@ -225,8 +294,12 @@ export class Explorer {
     return data.total;
   };
 
-  // get the fee rate of the tx in satoshis per byte (3 decimal places accuracy)
-  // use vout and vin from prepare_vout() and prepare_vin()
+  /**
+   * Calculate transaction fee
+   * @param vout - Prepared outputs
+   * @param vin - Prepared inputs
+   * @returns {Promise<number>} Total transaction fee in satoshis
+   */
   async calculate_fee(
     vout: PreparedTransactionOutputs,
     vin: PreparedTransactionInputs
@@ -235,7 +308,11 @@ export class Explorer {
     const totalVout = await this.calculate_total(vout);
     return (totalVin - totalVout);
   };
-
+  /**
+   * Prepare raw vout for database storage
+   * @param vout - Raw transaction `vout` array
+   * @returns Prepared outputs
+   */
   async prepare_vout(vout: TransactionOutput[]) {
     const data: {
       vout: PreparedTransactionOutputs,
@@ -262,7 +339,11 @@ export class Explorer {
 
     return data;
   };
-
+  /**
+   * Prepare raw vin for database storage
+   * @param tx - Raw transaction info
+   * @returns Prepared inputs
+   */
   async prepare_vin(tx: RawTransaction) {
     const { vin, vout } = tx;
     const data: {
@@ -283,7 +364,12 @@ export class Explorer {
 
     return data;
   };
-
+  /**
+   * Gather the address/amount used by vin
+   * @param vin - Raw vin
+   * @param vouts - Raw vout
+   * @returns Array of input addresses
+   */
   private async get_input_addresses(
     vin: TransactionInput,
     vouts: TransactionOutput[]
