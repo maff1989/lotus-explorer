8496c720a2ef766ae52b7724978f7dbbf264ba7d
maff1989 <maff1989@stamprepo.local>
2022-06-23 19:58:30 -0500

    test/database.ts: fix db connect string

--- test/database.ts
+++ test/database.ts
@@ -3,14 +3,14 @@ import { Database } from '../lib/database';
 
 
 const dbString = 'mongodb://' + settings.dbsettings.user
-+ ':' + settings.dbsettings.password
-+ '@' + settings.dbsettings.address
-+ ':' + settings.dbsettings.port
-+ '/' + settings.dbsettings.database;
+  + ':' + settings.dbsettings.password
+  + '@' + settings.dbsettings.address
+  + ':' + settings.dbsettings.port
+  + '/' + settings.dbsettings.database;
 const db = new Database();
 
 const main = async () => {
-  await db.connect('mongo');
+  await db.connect(dbString);
   const isLocked = await db.is_locked();
 
   console.log('isLocked', isLocked);
