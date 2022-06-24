a70e5ac40cb1c31853cea8890eeb8f21e606c91e
maff1989 <maff1989@stamprepo.local>
2022-06-23 19:56:46 -0500

    test/database.ts: set up initial testing

---
+++ test/database.ts
@@ -0,0 +1,19 @@
+const settings = require('../lib/settings');
+import { Database } from '../lib/database';
+
+
+const dbString = 'mongodb://' + settings.dbsettings.user
++ ':' + settings.dbsettings.password
++ '@' + settings.dbsettings.address
++ ':' + settings.dbsettings.port
++ '/' + settings.dbsettings.database;
+const db = new Database();
+
+const main = async () => {
+  await db.connect('mongo');
+  const isLocked = await db.is_locked();
+
+  console.log('isLocked', isLocked);
+  await db.disconnect();
+};
+main();
