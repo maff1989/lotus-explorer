029bae16431f75a766d0c8103c20005320ed5f2f
maff1989 <maff1989@stamprepo.local>
2022-06-23 20:19:49 -0500

    test/database.ts: add create_stats()

--- test/database.ts
+++ test/database.ts
@@ -13,9 +13,11 @@ const main = async () => {
   await db.connect(dbString);
   const isLocked = await db.is_locked();
   const stats = await db.get_stats('Lotus');
+  const newStats = await db.create_stats('homina');
 
   console.log('isLocked', isLocked);
   console.log('stats', stats);
+  console.log('newStats', newStats);
   await db.disconnect();
 };
 main();
