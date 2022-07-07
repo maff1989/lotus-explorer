2226b30ddd95f1ec6c25e336b88d5de2349e7230
maff1989 <maff1989@stamprepo.local>
2022-07-06 20:17:48 -0500

    app.ts: fix indentation for dev/prod error handlers

--- app.ts
+++ app.ts
@@ -322,21 +322,21 @@ app.use(async (req, res, next) => {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use((err, req, res, next) => {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 // production error handler
 // no stacktraces leaked to user
 app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 export default app;
