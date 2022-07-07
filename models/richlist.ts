1a17bf1890b5c8a4be20de346ca8857aca281c5e
maff1989 <maff1989@stamprepo.local>
2022-07-07 14:52:15 -0500

    models/richlist.ts: small formatting fix

--- models/richlist.ts
+++ models/richlist.ts
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 export default model('Richlist',
   new Schema({
     coin: { type: String },	
-    received: { type: Array, default: []},
+    received: { type: Array, default: [] },
     balance: { type: Array, default: [] },
   })
 );
