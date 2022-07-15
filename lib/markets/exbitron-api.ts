7fcf7a890635def9fdec8689a4733b5d4b7a4a40
maff1989 <maff1989@stamprepo.local>
2022-07-15 09:29:36 -0500

    Convert Exbitron market lib to TypeScript

---
+++ lib/markets/exbitron-api.ts
@@ -0,0 +1,45 @@
+export const BASE_URL = 'https://www.exbitron.com/api/v2/peatio/public'
+export const MARKETS_URL = 'markets';
+export type Tickers = {
+  at: string, // unix timestamp
+  ticker: {
+    low: string,
+    high: string,
+    last: string,
+    avg_price: string,
+    price_change_percent: string
+    volume: string, // EXCHANGE volume
+    amount: string, // COIN volume
+  }
+};
+export type Trade = {
+  id: number,
+  price: number,
+  amount: number,
+  total: number,
+  market: string,
+  created_at: number,
+  taker_type: 'buy' | 'sell'
+};
+export type Order = {
+  id: number,
+  uuid: string,
+  side: 'buy' | 'sell',
+  ord_type: 'limit',
+  price: string,
+  avg_price: string,
+  state: string,
+  market_type: 'spot',
+  created_at: string, // ISO date string
+  updated_at: string, //ISO date string
+  origin_volume: string,
+  remaining_volume: string,
+  executed_volume: string,
+  maker_fee: string,
+  taker_fee: string,
+  trades_count: number
+};
+export type OrderBook = {
+  asks: Order[],
+  bids: Order[]
+};
--- lib/markets/exbitron.ts
+++ lib/markets/exbitron.ts
@@ -1,62 +1,106 @@
 import request from 'request-promise';
 import settings from '../settings';
+import * as API from './exbitron-api';
 
-type Exchange = {
-  Orders: {},
-  Summary: {
-    bid: number,
-    ask: number,
-    volume: string,
-    volume_btc: string,
-    high: string,
-    low: string,
-    last: string,
-    change: number,
-  },
-  Trades: {},
-  Settings: {
-    coin: string,
-    exchange: string,
-    enabled: string[],
-    default: string,
-  },
-};
-type ExchangeOrders = {
-
-};
-type ExchangeTrades = {
-
+type Summary = {
+  low: number,
+  high: number,
+  last: number,
+  change: string,
+  bid: number,
+  ask: number,
+  volume_exchange: number,
+  volume_xpi: number,
 };
 
-const BASE_URL = 'https://www.exbitron.com/api/v2/peatio/public/markets/';
-const COIN = settings.coin;
-const EXCHANGE = settings.markets.exchange;
-
-export const get_data = async (settings: Exchange['Settings']) => {
-    
+type ParsedOrder = {
+  amount: number,
+  price: number,
+  total: number
 };
 
-const get_summary = async () => {
-  const { ticker: { vol, volume, high, low, last }} = await request.get({
-    uri: BASE_URL + 'xpiusdt/tickers',
-    json: true,
-  });
-  return {
-    bid: 0,
-    ask: 0,
-    change: 0,
-    volume: vol,
-    volume_btc: volume,
-    high,
-    low,
-    last,
-  } as Exchange['Summary'];
+export type ParsedData = {
+  stats: Summary,
+  trades: API.Trade[],
+  buys: ParsedOrder[],
+  sells: ParsedOrder[]
+  chartdata?,
 };
 
-const get_trades = async () => {
+export default class {
+  private TRADEPAIR =
+    settings.markets.coin.toLowerCase() +
+    settings.markets.exchange.toLowerCase();
+  private URI =
+    `${API.BASE_URL}/${API.MARKETS_URL}`
+    + `/${this.TRADEPAIR}`;
+  
+  async get_data(): Promise<ParsedData> {
+    const { buys, sells } = await this.get_orders();
+    return {
+      buys, sells,
+      chartdata: [],
+      trades: await this.get_trades(),
+      stats: await this.get_summary(),
+    }
+  };
 
-};
+  private async get_summary
+  (): Promise<Summary> {
+    try {
+      const { ticker }: API.Tickers = await request.get({
+        uri: this.URI + `/tickers`,
+        json: true,
+      });
+      return {
+        volume_exchange: Number(ticker.volume),
+        volume_xpi: Number(ticker.amount),
+        low: Number(ticker.low),
+        high: Number(ticker.high),
+        last: Number(ticker.last),
+        change: ticker.price_change_percent,
+        bid: 0,
+        ask: 0
+      };
+    } catch (e: any) {
+      throw new Error(`Exbitron.get_summary: ${e.message}`);
+    }
+  };
 
-const get_orders = async () => {
+  private async get_orders
+  (): Promise<{
+    'buys': ParsedOrder[],
+    'sells': ParsedOrder[]
+  }> {
+    const processor = (order: API.Order) => {
+      return {
+        amount: Number(order.remaining_volume),
+        price: Number(order.price),
+        total: Number(order.price) * Number(order.remaining_volume)
+      };
+    };
+    try {
+      const { bids, asks }: API.OrderBook = await request.get({
+        uri: this.URI + `/order-book?asks_limit=50&bid_limit=50`,
+        json: true,
+      }) || { bids: [], asks: [] };
+      const buys = bids.map(bid => processor(bid));
+      const sells = asks.map(ask => processor(ask));
+      return { buys, sells };
+    } catch (e: any) {
+      throw new Error(`Exbitron.get_orders: ${e.message}`);
+    }
+  };
 
+  private async get_trades
+  (): Promise<API.Trade[]> {
+    try {
+      return await request.get({
+        uri: this.URI + `/trades?limit=100`,
+        json: true,
+      });
+    } catch (e: any) {
+      throw new Error(`Exbitron.get_trades: ${e.message}`);
+    }
+  };
 };
