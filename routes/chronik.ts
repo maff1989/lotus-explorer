b050c2172f0f3f5c037d498e898a8aefb6609018
maff1989 <maff1989@stamprepo.local>
2022-09-01 20:44:46 -0500

    routes/chronik.ts: add more API endpoints
    Add `block`, `tx`, and `sendtx` (tx broadcast) endpoints
    Also fix the `address` processing logic

--- routes/chronik.ts
+++ routes/chronik.ts
@@ -11,18 +11,54 @@ const chronik = new Chronik(
 
 const chronikRouter = express.Router();
 chronikRouter.get('/utxos/:address', async (req, res) => {
-  const { address } = req.params;
+  const address = String(req.params.address);
   try {
     if (!Address.isValid(address)) {
       throw new Error('address is invalid');
     }
-    const decoded = Script.fromAddress(String(address));
-    const pkh = decoded.getPublicKeyHash().toString('hex');
-    const script = chronikClient.script('p2pkh', pkh);
-    return res.send(await script.utxos());
+    const script = Script.fromAddress(address);
+    const scriptType = chronik.getScriptType(script.toAddress());
+    const utxos = await chronik.client
+      .script(scriptType, script.getData().toString('hex'))
+      .utxos();
+    return res.send(utxos);
   } catch (e: any) {
     console.log(`${req.url}: ${e.message}`);
-    return res.send({ error: `failed to fetch UTXOs for ${address}` })
+    return res.send({ error: `failed to fetch UTXOs for ${address}` });
+  }
+});
+chronikRouter.get('/block/:hashOrHeight', async (req, res) => {
+  const { hashOrHeight } = req.params;
+  try {
+    const block = await chronik.client.block(
+      isNaN(Number(hashOrHeight))
+        ? hashOrHeight
+        : Number(hashOrHeight));
+    return res.send(block);
+  } catch (e: any) {
+    console.log(`${req.url}: ${e.message}`);
+    return res.send({ error: `failed to fetch block for ${hashOrHeight}` });
+  }
+});
+chronikRouter.get('sendtx/:rawTx', async (req, res) => {
+  const rawTx = String(req.params.rawTx);
+  const skipSlpCheck = true;
+  try {
+    const { txid } = await chronik.client.broadcastTx(rawTx, skipSlpCheck);
+    return res.send(txid);
+  } catch (e: any) {
+    console.log(`${req.url}: ${e.message}`);
+    return res.send({ error: `failed to send rawTx ${rawTx}` });
+  }
+});
+chronikRouter.get('/tx/:txid', async (req, res) => {
+  const txid = String(req.params.txid);
+  try {
+    const tx = await chronik.client.tx(txid);
+    return res.send(tx);
+  } catch (e: any) {
+    console.log(`${req.url}: ${e.message}`);
+    return res.send({ error: `failed to fetch transaction for ${txid}` });
   }
 });
 
