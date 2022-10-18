60f90b1bfb8f38d463aa6d820889101e31bd65fe
maff1989 <maff1989@stamprepo.local>
2022-10-18 13:14:39 -0500

    routes/chronik.ts: more handlers + small fixes
    - Add a `/history` endpoint
    - Use `res.json()` to send data to client
    - Incorporate changes from lib/chronik.ts
    - Misc logic/formatting changes

--- routes/chronik.ts
+++ routes/chronik.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Script, Address } from '@abcpros/bitcore-lib-xpi';
 import { Chronik } from '../lib/chronik';
 import settings from '../lib/settings';
 
@@ -13,31 +12,37 @@ const chronikRouter = express.Router();
 chronikRouter.get('/utxos/:address', async (req, res) => {
   const address = String(req.params.address);
   try {
-    if (!Address.isValid(address)) {
-      throw new Error('address is invalid');
-    }
-    const script = Script.fromAddress(address);
-    const scriptType = chronik.getScriptType(script.toAddress());
-    const utxos = await chronik.client
-      .script(scriptType, script.getData().toString('hex'))
-      .utxos();
-    return res.send(utxos);
+    const utxos = await chronik.addressGetUtxos(address);
+    return res.json(utxos);
   } catch (e: any) {
-    console.log(`${req.url}: ${e.message}`);
-    return res.send({ error: `failed to fetch UTXOs for ${address}` });
+    console.log(`${req.originalUrl}: ${e.message}`);
+    return res.json({ error: `failed to fetch UTXOs for ${address}` });
+  }
+});
+chronikRouter.get('/history/:address/:page?/:length?', async (req, res) => {
+  const { address, page, length } = req.params;
+  const p = page ? Number(page): 0;
+  const l = length ? Number(length): settings.txcount_per_page;
+  try {
+    const history = await chronik.addressGetHistory(address, p, l);
+    return res.json(history);
+  } catch (e: any) {
+    console.log(`${req.originalUrl}: ${e.message}`);
+    return res.json({ error: `failed to fetch history for ${address}` });
   }
 });
 chronikRouter.get('/block/:hashOrHeight', async (req, res) => {
   const { hashOrHeight } = req.params;
+  const height = Number(hashOrHeight);
   try {
     const block = await chronik.client.block(
-      isNaN(Number(hashOrHeight))
+      isNaN(height)
         ? hashOrHeight
-        : Number(hashOrHeight));
-    return res.send(block);
+        : height);
+    return res.json(block);
   } catch (e: any) {
-    console.log(`${req.url}: ${e.message}`);
-    return res.send({ error: `failed to fetch block for ${hashOrHeight}` });
+    console.log(`${req.originalUrl}: ${e.message}`);
+    return res.json({ error: `failed to fetch block for ${hashOrHeight}` });
   }
 });
 chronikRouter.get('sendtx/:rawTx', async (req, res) => {
@@ -45,20 +50,20 @@ chronikRouter.get('sendtx/:rawTx', async (req, res) => {
   const skipSlpCheck = true;
   try {
     const { txid } = await chronik.client.broadcastTx(rawTx, skipSlpCheck);
-    return res.send(txid);
+    return res.json(txid);
   } catch (e: any) {
-    console.log(`${req.url}: ${e.message}`);
-    return res.send({ error: `failed to send rawTx ${rawTx}` });
+    console.log(`${req.originalUrl}: ${e.message}`);
+    return res.json({ error: `failed to send rawTx ${rawTx}` });
   }
 });
 chronikRouter.get('/tx/:txid', async (req, res) => {
   const txid = String(req.params.txid);
   try {
     const tx = await chronik.client.tx(txid);
-    return res.send(tx);
+    return res.json(tx);
   } catch (e: any) {
-    console.log(`${req.url}: ${e.message}`);
-    return res.send({ error: `failed to fetch transaction for ${txid}` });
+    console.log(`${req.originalUrl}: ${e.message}`);
+    return res.json({ error: `failed to fetch transaction for ${txid}` });
   }
 });
 
